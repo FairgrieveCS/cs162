b84db6df0d8ff6db642a1109edc9d29991e89617
Jonathan Fairgrieve <fairgrij@oregonstate.edu>
2022-09-27 13:43:02 -0700

    completed project 0 - Jonathan Fairgrieve

---
+++ main.py
@@ -0,0 +1,12 @@
+''' Jonathan Fairgrieve
+Project 0
+CS 162 @ 8 AM
+fairgrij@oregonstate.edu | jonathan.fairgrieve.2778@mail.linnbenton.edu
+'''
+
+def addition(a, b):
+    sum = a + b
+    return sum
+
+
+print(addition(2, 3))
